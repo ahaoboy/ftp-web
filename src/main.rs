88531d15cd546f3183309418bbf560c6f535c342
ahaoboy <504595380@qq.com>
2024-06-12 15:46:11 +0800

    add clap

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,8 @@
 use axum::extract::{self, State};
 use axum::http::{header, StatusCode};
 use axum::response::{IntoResponse, Redirect};
+use clap::Parser;
+use fast_qr::{QRBuilder, ECL};
 use fileinfo::FileInfo;
 use std::str::FromStr;
 use std::sync::{Arc, Mutex};
@@ -14,10 +16,44 @@ struct AppState {
     ftp: Arc<Mutex<FtpStream>>,
 }
 
+#[derive(Parser, Debug, Clone)]
+#[command(author, version, about, long_about = None)]
+struct Cli {
+    /// name
+    #[arg(short, long, default_value_t = String::from(""))]
+    name: String,
+
+    /// password
+    #[arg(long, short, default_value_t = String::from(""))]
+    password: String,
+
+    /// host
+    #[arg(long, default_value_t = String::from("0.0.0.0"))]
+    host: String,
+
+    /// port
+    #[arg(long, default_value_t = 8080)]
+    port: u16,
+
+    /// ftp
+    #[clap()]
+    ftp: String,
+}
+
 #[tokio::main]
 async fn main() {
-    let mut ftp_stream = FtpStream::connect("192.168.0.64:7266").unwrap();
-    let _ = ftp_stream.login("pc", "111111").unwrap();
+    let cli = Cli::parse();
+    let Cli {
+        name,
+        password,
+        ftp,
+        port,
+        host,
+    } = cli.clone();
+
+    let mut ftp_stream = FtpStream::connect(&ftp).unwrap();
+    let _ = ftp_stream.login(name, password).expect("login error");
+
     let state = AppState {
         ftp: Arc::new(Mutex::new(ftp_stream)),
     };
@@ -25,13 +61,22 @@ async fn main() {
         .route("/", get(|| async { Redirect::permanent("/ftp/") }))
         .route("/ftp/*O", get(using_ftp))
         .route("/file/*O", get(using_file))
-        // .route("/*", get(|| async { Redirect::permanent("/ftp/") }))
         .with_state(state);
 
-    let listener = tokio::net::TcpListener::bind("127.0.0.1:3000")
+    let port = find_port::find_port("127.0.0.1", port).expect("find port error");
+    let local_ip = local_ip_address::local_ip().unwrap();
+
+    let listener = tokio::net::TcpListener::bind(format!("{host}:{port}"))
         .await
         .unwrap();
-    println!("listening on http://{}/", listener.local_addr().unwrap());
+
+    let s1 = format!("http://{}:{}/", "localhost", port);
+    let s2 = format!("http://{}:{}/", local_ip, port);
+    let s3 = format!("http://{}/{}/", host, port);
+    let qrcode = QRBuilder::new(s2.clone()).ecl(ECL::H).build().unwrap();
+    qrcode.print();
+    println!("ftp-web:\n{}\n{}\n{}", s1, s2, s3);
+
     axum::serve(listener, app).await.unwrap();
 }
 
@@ -60,7 +105,12 @@ async fn using_ftp(
 
 fn get_html(ftp: &Arc<Mutex<FtpStream>>, path: &str) -> String {
     let mut ftp = ftp.lock().unwrap();
-    let list = ftp.list(Some(&path)).or(ftp.list(None)).unwrap();
+
+    let (list, path) = if let Ok(list) = ftp.list(Some(&path)) {
+        (list, path)
+    } else {
+        ((ftp.list(None)).unwrap(), "")
+    };
 
     let li_text = list.iter().filter_map(|i| {
         let info: FileInfo = FileInfo::from_str(&i).ok()?;
@@ -75,6 +125,8 @@ fn get_html(ftp: &Arc<Mutex<FtpStream>>, path: &str) -> String {
 
     let li_text = li_text.collect::<Vec<_>>();
     let li_text = li_text.join("\n");
+
+    let path = if path.len() == 0 { "/" } else { path };
 
     let html = format!(
         r#"
